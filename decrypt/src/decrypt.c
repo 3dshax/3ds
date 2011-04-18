299b929c5ba4acb6f2977b31f0d246789d23ecc4
Erant <erantimus@gmail.com>
2011-04-18 14:23:18 -0700

    Added hashtable support, added sector-in-use parsing.

--- decrypt/src/decrypt.c
+++ decrypt/src/decrypt.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <save.h>
+#include <utils.h>
 #include <sys/stat.h>
 
 #define XORPAD_SIZE	512
@@ -37,11 +38,12 @@ int wearlevel(void* buffer, size_t size, void* dest){
 	// Find the start
 	int num_entries = (size / SECTOR_SIZE) - 1;
 	entries = calloc(num_entries, sizeof(*entries));
+	sectors = (struct long_sector_entry*)(buf + ((num_entries + 1) * sizeof(*entries)));
 
 	// Populate the base state
 	struct header_entry *header = (struct header_entry*)buf;
 	for(int i = 0; i < num_entries; i++){
-		entries[i].phys_sec = header[i].phys_sec & 0x7F;
+		entries[i].phys_sec = header[i].phys_sec;
 		entries[i].virt_sec = i;
 		memcpy(entries[i].chksums, header[i].chksums, sizeof(entries[0].chksums));
 	}
@@ -50,15 +52,17 @@ int wearlevel(void* buffer, size_t size, void* dest){
 	while(sectors->magic == SECTOR_MAGIC){
 		int sec = sectors->sector.virt_sec;
 		entries[sec] = sectors->sector;
+		entries[sec].phys_sec |= 0x80;
 		sectors++;
 	}
 
 #ifdef DEBUG
 	for(int i = 0; i < num_entries; i++){
-		printf("virt: %d phys: %d, cnt: %d, ", 
+		printf("virt: %d, phys: %d, cnt: %d,%s", 
 				entries[i].virt_sec,
-				entries[i].phys_sec,
-				entries[i].virt_realloc_cnt);
+				entries[i].phys_sec & 0x7F,
+				entries[i].virt_realloc_cnt,
+				entries[i].phys_sec & 0x80 ? " (in use) " : " ");
 		printf("chksums: ");
 		for(int j = 0; j < sizeof(entries[i].chksums); j++)
 			printf("%02X ", entries[i].chksums[j]);
@@ -67,8 +71,10 @@ int wearlevel(void* buffer, size_t size, void* dest){
 #endif /* DEBUG */
 
 	for(int i = 0; i < num_entries; i++){
-		if(entries[i].phys_sec * SECTOR_SIZE < size){
-			memcpy(&((uint8_t*)dest)[SECTOR_SIZE * i], &buf[SECTOR_SIZE * entries[i].phys_sec], SECTOR_SIZE);
+		if((entries[i].phys_sec & 0x7F) * SECTOR_SIZE < size){
+			memcpy( &((uint8_t*)dest)[SECTOR_SIZE * i],
+				&buf[SECTOR_SIZE * (entries[i].phys_sec & 0x7F)],
+				SECTOR_SIZE);
 		} else {
 			fprintf(stderr, "Illegal physical sector (%d) in blockmap (%d)\n", entries[i].phys_sec, i);
 		}
@@ -84,7 +90,7 @@ int wearlevel(void* buffer, size_t size, void* dest){
 				buf[(SECTOR_SIZE * entries[i].phys_sec) + j * 0x200 + 3]);
 			
 				FILE* out = fopen(filename, "wb");
-				fwrite(&buf[(SECTOR_SIZE * entries[i].phys_sec) + j * 0x200], 1, 0x200, out);
+				fwrite(&buf[(SECTOR_SIZE * (entries[i].phys_sec & 0x7F)) + j * 0x200], 1, 0x200, out);
 				fclose(out);
 			}
 		}
@@ -96,17 +102,19 @@ int wearlevel(void* buffer, size_t size, void* dest){
 
 void parse_fs(void*, char*);
 
-#define PART_BASE	0x2000
-#define FS_BASE_OFF	0x9C
-#define FS_LENGTH_OFF	0xA4
-#define FS_INDEX_OFF	0x39
-#define DIFI_LENGTH	0x130
+#define PART_BASE		0x2000
+#define HASH_TBL_LEN_OFF	0x9C
+#define PARTITION_LEN_OFF	0xA4
+#define FS_INDEX_OFF		0x39
+#define DIFI_LENGTH		0x130
 
 void parse_partitions(void* buf, char* target_dir){
 	char part_dir[256];
+	uint8_t hash[SHA256_DIGEST_LENGTH];
 	uint8_t* buffer = (uint8_t*)buf + 0x200;
 	uint32_t cur_part = PART_BASE;
 	uint32_t part_length = 0;
+	uint32_t hash_tbl_length = 0;
 
 	if(mkdir(target_dir, 0766) < 0){
 		fprintf(stderr, "Couldn't create dir %s\n", target_dir);
@@ -116,11 +124,23 @@ void parse_partitions(void* buf, char* target_dir){
 	chdir(target_dir);
 	while(!strncmp((char*)buffer, "DIFI", 4)){
 		snprintf(part_dir, sizeof(part_dir), "part-%d", buffer[FS_INDEX_OFF]);
-		cur_part += *((uint32_t*)&buffer[FS_BASE_OFF]);
-		part_length = *((uint32_t*)&buffer[FS_LENGTH_OFF]);
+		hash_tbl_length = *((uint32_t*)&buffer[HASH_TBL_LEN_OFF]);
+		part_length = *((uint32_t*)&buffer[PARTITION_LEN_OFF]);
+		for(int i = 0; i < hash_tbl_length; i += SHA256_DIGEST_LENGTH){
+			for(int j = 0; j < part_length; j += SECTOR_SIZE){
+				sha256(buf + j + cur_part + hash_tbl_length, (part_length - j > SECTOR_SIZE) ? SECTOR_SIZE : part_length - j, hash);
+				if(!memcmp(buf + i + cur_part, hash, sizeof(hash))){
+					printf("Block (%08X) matches entry %d (", j + cur_part + hash_tbl_length, i / SHA256_DIGEST_LENGTH);
+					for(int x = 0; x < SHA256_DIGEST_LENGTH; x++)
+						printf("%02x", hash[x]);
+					printf(")\n");
+				}
+			}
+		}
 #ifdef DEBUG
-		printf("Partition @ %06X (%06X)\n", cur_part, part_length);
+		printf("Partition @ %06X (%06X)\n", cur_part, part_length + hash_tbl_length);
 #endif /* DEBUG */
+		cur_part += hash_tbl_length;
 		parse_fs(buf + cur_part, part_dir);
 		cur_part += part_length;
 		buffer += DIFI_LENGTH;
@@ -247,7 +267,7 @@ int main(int argc, char** argv){
 	void* decrypt_ptr = buf + SECTOR_SIZE;
 	void* write_ptr = buf;
 
-	if(do_wearlevel){	
+	if(do_wearlevel){
 		wearlevel_buf = calloc(1, size - SECTOR_SIZE);
 		if(wearlevel(buf, size, wearlevel_buf) < 0)
 			goto exit;
